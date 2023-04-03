8f3dde5da7bcf76c433356c29a2b2a3b5579cf0b
Dao Vinh Phuc <daovinhphuc.1632@gmail.com>
2023-04-03 22:47:53 +0700

    Tao trang them lien he moi

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
+
 const routes = [
     {
         path: "/",
@@ -15,11 +16,18 @@ const routes = [
         path: "/contacts/:id",
         name: "contact.edit",
         component: () => import("@/views/ContactEdit.vue"),
-        props: true // Truyền các biến trong $route.params vào làm props
+        props: true
+    },
+    {
+        path: "/contacts/create",
+        name: "contact.add",
+        component: () => import("@/views/ContactAdd.vue"),
     },
 ];
+
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes,
 });
+
 export default router;
--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -10,13 +10,18 @@
             </h4>
             <ContactList v-if="filteredContactsCount > 0" :contacts="filteredContacts" v-model:activeIndex="activeIndex" />
             <p v-else>Không có liên hệ nào.</p>
+
             <div class="mt-3 row justify-content-around align-items-center">
                 <button class="btn btn-sm btn-primary" @click="refreshList()">
                     <i class="fas fa-redo"></i> Làm mới
                 </button>
+
+
                 <button class="btn btn-sm btn-success" @click="goToAddContact">
                     <i class="fas fa-plus"></i> Thêm mới
                 </button>
+
+
                 <button class="btn btn-sm btn-danger" @click="removeAllContacts">
                     <i class="fas fa-trash"></i> Xóa tất cả
                 </button>
@@ -25,7 +30,7 @@
         <div class="mt-3 col-md-6">
             <div v-if="activeContact">
                 <h4>
-                    Chi tiết Liên hệ
+                    Chi tiết liên hệ
                     <i class="fas fa-address-card"></i>
                 </h4>
                 <ContactCard :contact="activeContact" />
@@ -38,20 +43,23 @@
                 </router-link>
             </div>
         </div>
+
     </div>
 </template>
+
 <script>
-import ContactCard from "@/components/ContactCard.vue";
-import InputSearch from "@/components/InputSearch.vue";
-import ContactList from "@/components/ContactList.vue";
-import ContactService from "@/services/contact.service";
+import ContactCard from '../components/ContactCard.vue';
+import InputSearch from '../components/InputSearch.vue';
+import ContactList from '../components/ContactList.vue';
+import ContactService from '../services/contact.service';
+
 export default {
     components: {
         ContactCard,
         InputSearch,
         ContactList,
     },
-    // Đoạn mã xử lý đầy đủ sẽ trình bày bên dưới
+
     data() {
         return {
             contacts: [],
@@ -60,21 +68,18 @@ export default {
         };
     },
     watch: {
-        // Giám sát các thay đổi của biến searchText.
-        // Bỏ chọn phần tử đang được chọn trong danh sách.
         searchText() {
             this.activeIndex = -1;
         },
     },
     computed: {
-        // Chuyển các đối tượng contact thành chuỗi để tiện cho tìm kiếm.
         contactStrings() {
             return this.contacts.map((contact) => {
                 const { name, email, address, phone } = contact;
                 return [name, email, address, phone].join("");
             });
         },
-        // Trả về các contact có chứa thông tin cần tìm kiếm.
+
         filteredContacts() {
             if (!this.searchText) return this.contacts;
             return this.contacts.filter((_contact, index) =>
@@ -97,29 +102,33 @@ export default {
                 console.log(error);
             }
         },
+
         refreshList() {
             this.retrieveContacts();
             this.activeIndex = -1;
         },
+
         async removeAllContacts() {
-            if (confirm("Bạn muốn xóa tất cả Liên hệ?")) {
+            if (confirm("Bạn mún xóa tất cả liên hệ?")) {
                 try {
                     await ContactService.deleteAll();
-                    this.refreshList();
+                    this.refreshList;
                 } catch (error) {
                     console.log(error);
                 }
             }
         },
+
         goToAddContact() {
-            this.$router.push({ name: "contact.add" });
+            this.$router.push({ name: 'contact.add', });
         },
     },
     mounted() {
         this.refreshList();
-    },
+    }
 };
 </script>
+
 <style scoped>
 .page {
     text-align: left;
